cee6604dc2eb2b7a487fd74f5e3b165ed7064b94
thanhtung030400 <thanhtung030400@gmail.com>
2020-10-09 15:33:48 +0700

    update

--- 7. Biến, kiểu dữ liệu và toán tử/2.Bai_tap/ung_dung_chuyen_doi_tien_te/ung_dung_chuyen_doi_tien_te.js
+++ 7. Biến, kiểu dữ liệu và toán tử/2.Bai_tap/ung_dung_chuyen_doi_tien_te/ung_dung_chuyen_doi_tien_te.js
@@ -4,13 +4,10 @@ function covertCurrency() {
     let Tocurrency = document.getElementById("Tocurrency");
     let result = document.getElementById("result");
 
-    if(Fromcurrency.value == "vnd" && Tocurrency.value == "usd") {
+    if(Fromcurrency.value === "vnd" && Tocurrency.value === "usd") {
         result.value = amount / 23000;
-    }else if (Fromcurrency.value == "vnd" && Tocurrency.value == "vnd") {
-        result.value = amount * 1;
-    }else if (Fromcurrency.value == "usd" && Tocurrency.value == "usd") {
-        result.value = amount * 1;
-    } else {
+    }else if(Fromcurrency.value === "usd" && Tocurrency.value === "vnd"){
         result.value = amount*23000;
-    }
+    }else
+        result.value = amount
 }
